434450568f39db609af3cd101eb37d7230152614
Angelo Dell Acqua <angelo.dellacqua82@gmail.com>
2025-09-26 14:39:10 -0400

    style: remove LocationWatcher component and integrate its functionality into Home component

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,50 @@
-import LocationWatcher from './components/location-watcher';
+"use client";
+import { Station } from "./components/station";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useNearbyStations, LocStatus } from "./hooks/use-nearby-stations";
 
-export default async function Home() {
+export default function Home() {
+  const { status, locationData } = useNearbyStations();
+
+  if (status === LocStatus.NotSupported) {
+    return (
+      <div className="p-4 rounded shadow">
+        Geolocation is not supported by your browser.
+      </div>
+    );
+  }
+
+  if (locationData.length === 0) {
+    return (
+      <div className="p-4 rounded shadow">
+        <div className="flex justify-between items-center pb-4">
+          <h1 className="text-2xl font-bold mb-4 ">Loading nearby stations</h1>
+        </div>
+        <div className="flex flex-col gap-6">
+          <Skeleton className="h-[125px]  rounded-xl  border py-6 shadow-sm" />
+          <Skeleton className="h-[125px]  rounded-xl  border py-6 shadow-sm" />
+          <Skeleton className="h-[125px]  rounded-xl  border py-6 shadow-sm" />
+          <Skeleton className="h-[125px]  rounded-xl  border py-6 shadow-sm" />
+          <Skeleton className="h-[125px]  rounded-xl  border py-6 shadow-sm" />
+          <Skeleton className="h-[125px]  rounded-xl  border py-6 shadow-sm" />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <main>
-      <LocationWatcher />
+      <div className="p-4 rounded shadow">
+        <div className="flex justify-between items-center pb-4">
+          <h1 className="text-2xl font-bold mb-4 ">Nearby</h1>
+        </div>
+        <div className="flex flex-col gap-6">
+          {locationData &&
+            locationData.map((station) => (
+              <Station station={station} key={station.id} />
+            ))}
+        </div>
+      </div>
     </main>
   );
 }
